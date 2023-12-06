0a8dfe6c46b4bd8a446e3c1b686446bb5203224f
peifangchiang <chiang.peif@northeastern.edu>
2023-12-06 11:42:54 -0500

    add: enterprise

---
+++ src/Enterprise/Enterprise.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Enterprise;
+
+import Business.Organization.Organization;
+import Business.Organization.OrganizationDirectory;
+import Business.UserAccount.UserAccount;
+
+/**
+ *
+ * @author zhaoyinhao
+ */
+public class Enterprise{
+    
+    private String enterpriseType;
+    private OrganizationDirectory organizationDirectory;
+    private String name;
+    private String email;
+    private String address;
+    private UserAccount acc;
+
+    public UserAccount getAcc() {
+        return acc;
+    }
+    
+    
+    public String getEnterpriseType() {
+        return enterpriseType;
+    }
+
+    public void setEnterpriseType(String enterpriseType) {
+        this.enterpriseType = enterpriseType;
+    }
+
+    public OrganizationDirectory getOrganizationDirectory() {
+        return organizationDirectory;
+    }
+
+    public void setOrganizationDirectory(OrganizationDirectory organizationDirectory) {
+        this.organizationDirectory = organizationDirectory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    
+    
+}
